efdc1ecf7296919d87f9b65c9ef808c8847b037c
LevyMia25 <tomlevyzschoche@gmail.com>
2024-06-28 12:29:20 +0200

    SiegerWinner

--- WinnerScreen.py
+++ WinnerScreen.py
@@ -29,23 +29,21 @@ goal_x = screen_width // 2 - goal_width // 2
 goal_y = screen_height // 2 - goal_height // 2
 
 # Schriftart
-font_path = "Assets/Schrift/Pixellari.ttf"  # Pfad zur heruntergeladenen Schriftart
+font_path = "Assets/Schrift/Pixellari.ttf"  
 font = pygame.font.Font(font_path, 65)
 font_button = pygame.font.Font(font_path, 50)
 
 button_width = 300
 button_height = 100
-button_spacing = 10  # Abstand zwischen den Buttons
-button_y = screen_height // 2
+button_spacing = 10  
+button_y = screen_height // 1.2
 button_menu_x = screen_width // 2 - (1.5 * button_width + button_spacing)
 button_neustart_x = screen_width // 2 - (0.5 * button_width)
 button_quit_x = screen_width // 2 + (0.5 * button_width + button_spacing)
 
-# Spielerbilder laden und skalieren
-player1_image = pygame.image.load('Assets/Bilder/Spieler/Fisch.png')  # Pfad zum Bild von Spieler 1
-player2_image = pygame.image.load('Assets/Bilder/Spieler/Fisch2.png')  # Pfad zum Bild von Spieler 2
+player1_image = pygame.image.load('Assets/Bilder/Spieler/Fisch.png')  
+player2_image = pygame.image.load('Assets/Bilder/Spieler/Fisch2.png')  
 
-# Neue Größe für die Spielerbilder
 new_player_width = 250
 new_player_height = 250
 
@@ -53,11 +51,12 @@ player1_image = pygame.transform.scale(player1_image, (new_player_width, new_pla
 player2_image = pygame.transform.scale(player2_image, (new_player_width, new_player_height))
 player_width, player_height = player1_image.get_size()
 
-# Funktion zur Anzeige der Gewinnnachricht und Buttons
-def display_winner(winner):
+# Funktion zur Anzeige der Gewinnnachricht und des Gewinnerbilds
+def display_winner(winner, winner_image):
     winner_text = font.render(f"{winner} hat gewonnen!", True, BLACK)
     screen.fill(WHITE)
     screen.blit(winner_text, (screen_width // 2 - winner_text.get_width() // 2, screen_height // 4 - winner_text.get_height() // 2))
+    screen.blit(winner_image, (screen_width // 2 - winner_image.get_width() // 2, screen_height // 2 - winner_image.get_height() // 2))
 
     # Buttons
     draw_buttons()
@@ -161,10 +160,10 @@ while True:
 
         # Überprüfen, ob ein Spieler das Ziel erreicht hat
         if player1_rect.colliderect(goal):
-            display_winner("Spieler 1")
+            display_winner("Spieler 1", player1_image)
             running = False
         if player2_rect.colliderect(goal):
-            display_winner("Spieler 2")
+            display_winner("Spieler 2", player2_image)
             running = False
 
         pygame.display.flip()
