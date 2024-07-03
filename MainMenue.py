9a69df1b4df3ce072748a5e9f548b5438f6b413e
LevyMia25 <tomlevyzschoche@gmail.com>
2024-07-03 13:30:04 +0200

    Map Auswahl

--- MainMenue.py
+++ MainMenue.py
@@ -113,14 +113,14 @@ class StartWindow:
 
             # Zeichnen des Start-Buttons mit Hover-Effekt
             mouse_pos = pygame.mouse.get_pos()
-            self.draw_button(self.start_game_button_rect, mouse_pos)
+            self.draw_button(self.start_game_button_rect, self.start_game_button_image, self.start_game_button_hover_image, mouse_pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if self.start_game_button_rect.collidepoint(mouse_pos):
-                        self.start_game()
+                        self.open_map_selection()
                         running = False
 
             pygame.display.flip()
@@ -128,45 +128,48 @@ class StartWindow:
 
         pygame.quit()
 
-    def draw_button(self, rect, mouse_pos):
+    def draw_button(self, rect, image, hover_image, mouse_pos):
         if rect.collidepoint(mouse_pos):
-            self.screen.blit(self.start_game_button_hover_image, rect.topleft)
+            self.screen.blit(hover_image, rect.topleft)
         else:
-            self.screen.blit(self.start_game_button_image, rect.topleft)
+            self.screen.blit(image, rect.topleft)
 
-    def start_game(self):
-        subprocess.Popen([sys.executable, 'game.py'])
+    def open_map_selection(self):
+        map_selection_window = MapSelectionWindow(self.main_menu)
+        map_selection_window.run()
 
-class OptionsWindow:
+class MapSelectionWindow:
     def __init__(self, main_menu):
         self.main_menu = main_menu
-        self.music_on = True  # Zustand des Musik-Buttons
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption('Options Window')
+        pygame.display.set_caption('Map Selection Window')
 
-        # Hintergrundbild für das Optionsfenster laden
-        self.background_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/OptionsBackground.png')
+        # Hintergrundbild für das Karten-Auswahl-Fenster laden
+        self.background_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/MapsBackground.jpg')
         self.background_image = pygame.transform.scale(self.background_image, (WIDTH, HEIGHT))
 
-        # Button-Bilder für Musik-Button laden und skalieren
-        self.music_on_button_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/MusikAN1.png')
-        self.music_on_button_image = pygame.transform.scale(self.music_on_button_image, (400, 100))
-        self.music_on_button_hover_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/MusikAN2.png')
-        self.music_on_button_hover_image = pygame.transform.scale(self.music_on_button_hover_image, (400, 100))
+        # Button-Bilder für Karten-Auswahl-Buttons laden und skalieren
+        self.map1_button_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/Schnee1.png')
+        self.map1_button_image = pygame.transform.scale(self.map1_button_image, (400, 100))
+        self.map1_button_hover_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/Schnee2.png')
+        self.map1_button_hover_image = pygame.transform.scale(self.map1_button_hover_image, (400, 100))
 
-        self.music_off_button_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/MusikAUS1.png')
-        self.music_off_button_image = pygame.transform.scale(self.music_off_button_image, (400, 100))
-        self.music_off_button_hover_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/MusikAUS2.png')
-        self.music_off_button_hover_image = pygame.transform.scale(self.music_off_button_hover_image, (400, 100))
+        self.map2_button_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/Vulkan1.png')
+        self.map2_button_image = pygame.transform.scale(self.map2_button_image, (400, 100))
+        self.map2_button_hover_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/Vulkan2.png')
+        self.map2_button_hover_image = pygame.transform.scale(self.map2_button_hover_image, (400, 100))
 
-        self.music_button_rect = self.music_on_button_image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        self.map3_button_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/Space1.png')
+        self.map3_button_image = pygame.transform.scale(self.map3_button_image, (400, 100))
+        self.map3_button_hover_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/Space2.png')
+        self.map3_button_hover_image = pygame.transform.scale(self.map3_button_hover_image, (400, 100))
 
-        # Button-Bilder für Zurück-Button laden und skalieren
-        self.back_button_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/Zurück1.png')
-        self.back_button_image = pygame.transform.scale(self.back_button_image, (400, 100))
-        self.back_button_hover_image = pygame.image.load('Assets/Bilder/Hintergrund/Menü/Zurück2.png')
-        self.back_button_hover_image = pygame.transform.scale(self.back_button_hover_image, (400, 100))
-        self.back_button_rect = self.back_button_image.get_rect(center=(WIDTH // 2, HEIGHT // 2 + 150))
+        button_gap = 150
+        button_y = HEIGHT // 2 - 0.5 * button_gap
+
+        self.map1_button_rect = self.map1_button_image.get_rect(center=(WIDTH // 2, button_y))
+        self.map2_button_rect = self.map2_button_image.get_rect(center=(WIDTH // 2, button_y + button_gap))
+        self.map3_button_rect = self.map3_button_image.get_rect(center=(WIDTH // 2, button_y + 2 * button_gap))
 
     def run(self):
         clock = pygame.time.Clock()
@@ -177,44 +180,34 @@ class OptionsWindow:
 
             # Zeichnen der Buttons mit Hover-Effekt
             mouse_pos = pygame.mouse.get_pos()
-            self.draw_button(self.music_button_rect, mouse_pos)
-            self.draw_back_button(mouse_pos)
+            self.draw_button(self.map1_button_rect, self.map1_button_image, self.map1_button_hover_image, mouse_pos)
+            self.draw_button(self.map2_button_rect, self.map2_button_image, self.map2_button_hover_image, mouse_pos)
+            self.draw_button(self.map3_button_rect, self.map3_button_image, self.map3_button_hover_image, mouse_pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                    self.main_menu.run()
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.music_button_rect.collidepoint(mouse_pos):
-                        self.toggle_music()
-                    elif self.back_button_rect.collidepoint(mouse_pos):
-                        running = False
-                        self.main_menu.run()
+                    if self.map1_button_rect.collidepoint(mouse_pos):
+                        self.start_game()
+                    elif self.map2_button_rect.collidepoint(mouse_pos):
+                        self.start_game()
+                    elif self.map3_button_rect.collidepoint(mouse_pos):
+                        self.start_game()
 
             pygame.display.flip()
             clock.tick(FPS)
 
-    def draw_button(self, rect, mouse_pos):
-        if self.music_on:
-            image = self.music_on_button_image
-            hover_image = self.music_on_button_hover_image
-        else:
-            image = self.music_off_button_image
-            hover_image = self.music_off_button_hover_image
+        pygame.quit()
 
+    def draw_button(self, rect, image, hover_image, mouse_pos):
         if rect.collidepoint(mouse_pos):
             self.screen.blit(hover_image, rect.topleft)
         else:
             self.screen.blit(image, rect.topleft)
 
-    def draw_back_button(self, mouse_pos):
-        if self.back_button_rect.collidepoint(mouse_pos):
-            self.screen.blit(self.back_button_hover_image, self.back_button_rect.topleft)
-        else:
-            self.screen.blit(self.back_button_image, self.back_button_rect.topleft)
-
-    def toggle_music(self):
-        self.music_on = not self.music_on
+    def start_game(self):
+        subprocess.Popen([sys.executable, 'game.py'])
 
 if __name__ == '__main__':
     main_menu = MainMenu()
